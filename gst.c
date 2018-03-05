e12e7446d237c772e89e6493c1eb0dbb1986354a
Brett Heithold <baheithold@crimson.ua.edu>
2018-03-05 10:52:38 -0600

    Created GVAL struct and GVAL constructor

--- gst.c
+++ gst.c
@@ -4,6 +4,26 @@
  *  Description:
  */
 
+
+#include <stdio.h>
+#include <stdlib.h>
 #include "gst.h"
 #include "bst.h"
 
+
+typedef struct gval {
+    void *key;
+    int frequency;
+    void (*display)(void *, FILE *);
+    void (*compare)(void *, void *);
+} GVAL;
+
+
+GVAL *newGVAL(void *key, void (*d)(void *, FILE *), void (*c)(void *, void *)) {
+    GVAL *rv = malloc(sizeof(GVAL));
+    rv->key = key;
+    rv->frequency = 0;
+    rv->display = d;
+    rv->compare = c;
+    return rv;
+}
